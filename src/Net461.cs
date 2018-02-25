c81962e8bd1f092e9b6a40b3b0f190bacea72ed0
David Federman <david.federman@outlook.com>
2018-02-24 19:02:51 -0800

    Compile for net461 too

---
+++ src/Net461.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="Net461.cs" company="David Federman">
+// Copyright (c) David Federman. All rights reserved.
+// </copyright>
+
+#if NET461
+namespace ReferenceTrimmer
+{
+    using System.Collections.Generic;
+
+    internal static class Net461
+    {
+        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer) => new HashSet<TSource>(source, comparer);
+    }
+}
+#endif
