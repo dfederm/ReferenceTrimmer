814d09863cb15e2aa29373569eb48989efa6b2b1
David Federman <david.federman@outlook.com>
2023-01-24 21:46:06 -0800

    Avoid references added by Microsoft.NET.Build.Extensions

---
+++ test/TestData/BuildExtensions/Library/Library.cs
@@ -0,0 +1,9 @@
+﻿namespace Library
+{
+    public static class Foo
+    {
+        public static string Bar() => Dependency.Foo.Bar();
+
+        public static string Baz() => Newtonsoft.Json.JsonConvert.SerializeObject(null);
+    }
+}
