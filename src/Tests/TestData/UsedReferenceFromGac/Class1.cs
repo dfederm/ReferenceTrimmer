6e29522e93524ebef281ca90cdddd5222c832b6a
dfederm <dfederm@microsoft.com>
2024-03-05 10:27:00 -0800

    Add tests for references from the GAC

---
+++ src/Tests/TestData/UsedReferenceFromGac/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Application CreateApplication() => new Application();
+    }
+}
